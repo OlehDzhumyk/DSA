f41a4641a0039ce93f9cf6d46ab2d03113adfeef
OlehDzhumyk <Oleh.Dzhumyk.IR.2021@lpnu.ua>
2022-09-28 12:40:44 +0100

    Add new inputs.

--- Lab2/СycleDetector/src/main/java/ua/lviv/iot/Main.java
+++ Lab2/СycleDetector/src/main/java/ua/lviv/iot/Main.java
@@ -8,17 +8,17 @@ public class Main {
     public static void main(String[] args) {
 
         GraphReader reader = new GraphReader();
-        Graph graph = reader.setGraphFrom("src/main/resources/input.txt");
+        Graph graph1 = reader.setGraphFrom("src/main/resources/input1.txt");
+        Graph graph2 = reader.setGraphFrom("src/main/resources/input2.txt");
+        Graph graph3 = reader.setGraphFrom("src/main/resources/input3.txt");
+        Graph graph4 = reader.setGraphFrom("src/main/resources/input4.txt");
+        Graph graph5 = reader.setGraphFrom("src/main/resources/input5.txt");
 
         CycleDetector cycleDetector = new CycleDetector();
 
-        cycleDetector.printCycle(graph);
-
+        cycleDetector.printCycle(graph1);
 
 
     }
-
-
-
 
 }
